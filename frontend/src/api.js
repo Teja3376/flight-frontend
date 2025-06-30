1d97adc4c034356682c506caaaa8a4870dab0621
Teja3376 <kcteja62892@gmail.com>
2025-06-30 23:24:37 +0530

    new deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "https://flight-backend-production.up.railway.app/api",
   // baseURL: "http://localhost:5000/api/",
-  baseURL: "https://flight-backend-production.up.railway.app/api",
+  baseURL: "https://flight-backend-36uz.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
